2fb5929b96c72dd7d8a3ba12742c4c18d07fdca4
RockyLatchman <r.latchman3624@gmail.com>
2025-10-14 18:49:06 +0700

    HTML for header and footer template

--- public/index.php
+++ public/index.php
@@ -1,26 +1,26 @@
 <?php
 
-include_once('../templates/header.php');
+include_once '../templates/header.php';
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'products';
 
 switch($page):
    case 'product':
-      include_once('../templates/product.php');
+      include_once '../templates/product.php';
       break;
    case 'cart':
-      include_once('../templates/cart.php');
+      include_once '../templates/cart.php';
       break;
    case 'checkout':
-      include_once('../templates/checkout.php');
+      include_once '../templates/checkout.php';
       break;
    case 'confirmation':
-      include_once('../templates/confirmation.php');
+      include_once '../templates/confirmation.php';
       break;
    default:
-      include_once('../templates/products.php');
+      include_once '../templates/products.php';
 endswitch;
 
-
+include_once '../templates/footer.php';
 
 ?>
---
+++ templates/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+ <html lang="en">
+  <head>
+    <title>ShoeMart - PHP Cart</title>
+    <link rel="stylesheet" href="./public/css/main.css">
+    <script src="./public/js/main.js"></script>
+  </head>
+  <body>
